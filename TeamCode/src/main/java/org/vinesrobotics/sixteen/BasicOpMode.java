45642c272b31db5df7f26c1d8b11980e41a07ace
Vines Robotics <vinesrobotics@gmail.com>
2016-09-30 18:09:54 -0500

    Added Basic OpMode that doesn't do anything without the robot controller because the devs of this app are stupid

---
+++ TeamCode/src/main/java/org/vinesrobotics/sixteen/BasicOpMode.java
@@ -0,0 +1,17 @@
+package org.vinesrobotics.sixteen;
+
+import com.qualcomm.robotcore.eventloop.opmode.*;
+
+/**
+ * Created by Vines HS Robotics on 9/30/2016.
+ */
+
+@Autonomous(name="Basic")
+public class BasicOpMode extends OpMode {
+    public void init(){
+        telemetry.addLine("Op mode initialized");
+    }
+    public void loop(){
+
+    }
+}
